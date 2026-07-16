a4bbebcc4d811c7cfdcb393ca676deb2bd2889d5
Sudheshna10 <mammu031099@gmail.com>
2026-07-16 19:56:27 +0530

    User Card

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,8 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("sudheshna@gmail.com ");
-  const [password, setPassword] = useState("Sudheshna@10*");
+  const [emailId, setEmailId] = useState("mammu@gmail.com ");
+  const [password, setPassword] = useState("Mammu@10*");
   const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
