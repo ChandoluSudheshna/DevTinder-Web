b3efbac3914df9c59153b16b82cf41222b025a29
Sudheshna10 <mammu031099@gmail.com>
2026-07-16 19:25:35 +0530

    Logout feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,7 @@ import { BASE_URL } from "../utils/constants";
 const Login = () => {
   const [emailId, setEmailId] = useState("sudheshna@gmail.com ");
   const [password, setPassword] = useState("Sudheshna@10*");
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -24,6 +25,7 @@ const Login = () => {
       dispatch(addUser(res.data));
       navigate("/");
     } catch (err) {
+      setError(err?.response?.data || "Something went wrong");
       console.log(err);
     }
   };
@@ -55,7 +57,8 @@ const Login = () => {
               />
             </fieldset>
           </div>
-          <div className="card-actions justify-center">
+          <p className="text-red-500">{error}</p>
+          <div className="card-actions justify-center py-2">
             <button className="btn btn-primary" onClick={handleLogin}>
               Login
             </button>
